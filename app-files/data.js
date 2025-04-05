57451aed19deeca9a96adcba4d6902c2a52a5240
syifaqq29 <m-9675450@moe-dl.edu.my>
2025-04-05 23:44:49 +0800

    Add files via upload

--- app-files/data.js
+++ app-files/data.js
@@ -1,1 +1,75 @@
-
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-4_le_upscale_balanced_x4",
+      "name": "4_LE_upscale_balanced_x4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.8955313183898408,
+          "pitch": -0.016540392787431912,
+          "title": "Ruang Guru Pusat Sumber",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.2912467042946396,
+          "pitch": 0.07842315849637949,
+          "title": "Kaunter Pusat Sumber",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.5819077771349095,
+          "pitch": -0.006616157114972765,
+          "title": "Business Hub",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9189087033477445,
+          "pitch": 0.0033835197574862974,
+          "title": "Ruang Istiharat",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.9834640795817124,
+          "pitch": 0.047467228442883425,
+          "title": "Pusat Akses",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6129451309003713,
+          "pitch": 0.06370493272334699,
+          "title": "Rak Buku Pusat Sumber",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
